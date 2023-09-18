5c0cb8695486404136b54a5cbf4c895806e13d6c
AntonioRusan <antonrusan@gmail.com>
2023-09-18 23:12:33 +0300

    first commit

---
+++ Lab1_Text_Question_Answerer/BertModelLibrary/BertModel.cs
@@ -0,0 +1,170 @@
+﻿using BERTTokenizers;
+using Microsoft.ML.Data;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BertModelLibrary
+{
+    public class BertModel
+    {
+        private InferenceSession session;
+        static Semaphore sessionSemaphore = new Semaphore(1, 1);
+        public BertModel()
+        {
+            string modelPath = "..\\..\\..\\bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
+            string downaloadedModelPath = modelPath;
+
+            if (!File.Exists(modelPath))
+            {
+                var downloadTask = DownloadModel(modelPath);
+                downaloadedModelPath = downloadTask.Result;
+            }
+            session = new InferenceSession(downaloadedModelPath);
+        }
+
+        public static async Task<string> DownloadModel(string modelPath)
+        {
+            try
+            {
+                string modelWebSource = "https://storage.yandexcloud.net/dotnet4/bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
+                var httpClient = new HttpClient();
+                int retriesRemain = 5;
+                bool isDownloaded = false;
+                while (retriesRemain > 0 && !isDownloaded)
+                {
+                    try
+                    {
+                        using var stream = await httpClient.GetStreamAsync(modelWebSource);
+                        using var fileStream = new FileStream(modelPath, FileMode.CreateNew);
+                        await stream.CopyToAsync(fileStream);
+                        isDownloaded = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        retriesRemain--;
+                    }
+                }
+                if (!File.Exists(modelPath))
+                {
+                    throw new Exception($"Failed to download {modelWebSource}, retries expired.");
+                }
+                return modelPath;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<String> AnswerQuestionAsync(string text, string question, CancellationToken token)
+        {
+            try
+            {
+                sessionSemaphore.WaitOne();
+
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+                var sentence = $"{{\"question\": {question}, \"context\": \"@CTX\"}}".Replace("@CTX", text);
+                //Console.WriteLine(sentence);
+
+                // Create Tokenizer and tokenize the sentence.
+                var tokenizer = new BertUncasedLargeTokenizer();
+
+                // Get the sentence tokens.
+                var tokens = tokenizer.Tokenize(sentence);
+                // Console.WriteLine(String.Join(", ", tokens));
+
+                // Encode the sentence and pass in the count of the tokens in the sentence.
+                var encoded = tokenizer.Encode(tokens.Count(), sentence);
+
+                // Break out encoding to InputIds, AttentionMask and TypeIds from list of (input_id, attention_mask, type_id).
+                var bertInput = new BertInput()
+                {
+                    InputIds = encoded.Select(t => t.InputIds).ToArray(),
+                    AttentionMask = encoded.Select(t => t.AttentionMask).ToArray(),
+                    TypeIds = encoded.Select(t => t.TokenTypeIds).ToArray(),
+                };
+
+                // Create input tensor.
+
+                var input_ids = ConvertToTensor(bertInput.InputIds, bertInput.InputIds.Length);
+                var attention_mask = ConvertToTensor(bertInput.AttentionMask, bertInput.InputIds.Length);
+                var token_type_ids = ConvertToTensor(bertInput.TypeIds, bertInput.InputIds.Length);
+
+
+                // Create input data for session.
+                var input = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("input_ids", input_ids),
+                                                    NamedOnnxValue.CreateFromTensor("input_mask", attention_mask),
+                                                    NamedOnnxValue.CreateFromTensor("segment_ids", token_type_ids) };
+
+                // Run session and send the input data in to get inference output. 
+                var output = await Task.Run(() => session.Run(input));
+
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+
+                // Call ToList on the output.
+                // Get the First and Last item in the list.
+                // Get the Value of the item and cast as IEnumerable<float> to get a list result.
+                List<float> startLogits = (output.ToList().First().Value as IEnumerable<float>).ToList();
+                List<float> endLogits = (output.ToList().Last().Value as IEnumerable<float>).ToList();
+
+                // Get the Index of the Max value from the output lists.
+                var startIndex = startLogits.ToList().IndexOf(startLogits.Max());
+                var endIndex = endLogits.ToList().IndexOf(endLogits.Max());
+
+                // From the list of the original tokens in the sentence
+                // Get the tokens between the startIndex and endIndex and convert to the vocabulary from the ID of the token.
+                var predictedTokens = tokens
+                            .Skip(startIndex)
+                            .Take(endIndex + 1 - startIndex)
+                            .Select(o => tokenizer.IdToToken((int)o.VocabularyIndex))
+                            .ToList();
+
+                // Print the result.
+
+                await Task.Delay(3000);
+
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+                sessionSemaphore.Release();
+                return String.Join(" ", predictedTokens);
+            }
+            catch (Exception)
+            {
+                sessionSemaphore.Release();
+                throw;
+            }
+
+        }
+
+        public static Tensor<long> ConvertToTensor(long[] inputArray, int inputDimension)
+        {
+            // Create a tensor with the shape the model is expecting. Here we are sending in 1 batch with the inputDimension as the amount of tokens.
+            Tensor<long> input = new DenseTensor<long>(new[] { 1, inputDimension });
+
+            // Loop through the inputArray (InputIds, AttentionMask and TypeIds)
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                // Add each to the input Tenor result.
+                // Set index and array value of each input Tensor.
+                input[0, i] = inputArray[i];
+            }
+            return input;
+        }
+    }
+
+    public class BertInput
+    {
+        public long[] InputIds { get; set; }
+        public long[] AttentionMask { get; set; }
+        public long[] TypeIds { get; set; }
+    }
+}
---
+++ Lab1_Text_Question_Answerer/Lab1_Text_Question_Answerer/Program.cs
@@ -0,0 +1,89 @@
+﻿using BertModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab1_Text_Question_Answerer
+{
+    class Program
+    {
+        static Semaphore consoleSemaphore = new Semaphore(1, 1);
+        static async Task Main(string[] args)
+        {
+
+            try
+            {
+                if (args.Length > 0)
+                {
+                    string path = @args[0];
+                    //string path = "..\\..\\..\\..\\hobbit.txt";
+                    string text = GetTextFromFile(path);
+                    Console.WriteLine(text);
+
+                    CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+                    CancellationToken token = cancelTokenSource.Token;
+
+                    var bertModel = new BertModel();
+                    string? question = "start";
+                    consoleSemaphore.WaitOne();
+                    while ((question = Console.ReadLine()) != "")
+                    {
+                        consoleSemaphore.Release();
+                        if (question == "cancel") { cancelTokenSource.Cancel(); }
+                        var answer = Task.Run(() => ProcessQuestionAsync(bertModel, text, question, token));
+                        consoleSemaphore.WaitOne();
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("No file path in command line arguments!");
+                }
+            }
+            catch (Exception ex)
+            {
+                consoleSemaphore.WaitOne();
+                Console.WriteLine(ex.Message);
+                consoleSemaphore.Release();
+            }
+
+        }
+        static async Task ProcessQuestionAsync(BertModel bertModel, string text, string question, CancellationToken token)
+        {
+            try
+            {
+                var answer = await Task.Run(() => bertModel.AnswerQuestionAsync(text, question, token));
+                consoleSemaphore.WaitOne();
+                Console.WriteLine(question + " : " + answer);
+                consoleSemaphore.Release();
+            }
+            catch (Exception ex)
+            {
+                consoleSemaphore.WaitOne();
+                Console.WriteLine(question + " : " + ex.Message);
+                consoleSemaphore.Release();
+            }
+        }
+
+        static string GetTextFromFile(string path)
+        {
+            StreamReader? reader = null;
+            try
+            {
+                reader = new StreamReader(path);
+                string text = reader.ReadToEnd();
+                return text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception loading from file: {ex.Message}");
+                return "";
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Dispose();
+            }
+        }
+    }
+}
